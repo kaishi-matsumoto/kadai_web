6222030368ee7c197076f2e228f055428daa4ec2
kaishi-matsumoto <kai19940619shi@gmail.com>
2021-01-18 13:04:23 +0900

    deleteメソッドによるid取得、および照合結果による対象配列の削除まで成功

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { useEffect ,useState } from 'react';
+/* import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+} from 'react-router-dom';
+ */
 
 function App() {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [datas, setDatas] = useState([]);
   const [isDeleting, setIsDeleting] = useState(false)
-  
+  const [isLoading, setIsLoading] = useState(false)
 
 
   useEffect(() => {
@@ -32,12 +39,11 @@ function App() {
       )
   }, [])
 
-  /* const submitDelete=(e)=>{
-          console.log(e)
+  const clickDelete=(e,index)=>{
           
           if(!isDeleting){
-            setIsDeleting(true);
-            fetch("https://api.manage.prona.com/client/"+datas[e].id ,{
+            setIsLoading(true)
+            fetch("https://api.manage.prona.com/client/"+datas[index].id ,{
               method: 'DELETE',
                 headers: {
                   'Content-Type': 'application/json',
@@ -45,20 +51,31 @@ function App() {
                 },
                 mode: 'cors',
             })
-            .then(response=> response.text())
-            .then((result)=>{
-              console.log(result);
-              if(result === datas[0].id){
+            .then(response => response.json())
+            .then((result) => {              
+              
+              if(result.id===datas[index].id ){
                 const deleteData = datas.slice();
-                deleteData.splice(e,1);
+                deleteData.splice(index,1);
                 setDatas(deleteData);
-                setIsDeleting(true)
-                e.preventDefault();
+                setIsLoading(false)
+                setIsDeleting(false)
+              }else{
+                console.log(datas[index])
+                console.log(result.id===datas[index].id ? 'OK': 'NO')
+                setIsLoading(false)
+                setIsLoading(false)
               }
+
             })
+            
+            }else{
+              setIsDeleting(false)
             }
 
-  } */
+            
+
+  }
       if (error) {
         return <div>Error: {error.message}</div>;
       } else if (!isLoaded) {
@@ -67,43 +84,29 @@ function App() {
       } else {
         
       return <div>
-        {datas.map((item, index) => (
-          <form key={index} 
-          onSubmit={(e)=>{
-            console.log(datas[index])
-            fetch("https://api.manage.prona.com/client/"+datas[index].id ,{
-            method: 'DELETE',
-              headers: {
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': 'http://localhost:3000', 
-              },
-              mode: 'cors',
-          })
-          .then(response=> response.text())
-          .then((result)=>{
-            console.log(result);
-            if(result === datas[index].id){
-              setIsDeleting(false)
-              const deleteData = datas.slice();
-              deleteData.splice(index,1);
-              setDatas(deleteData);
-              setIsDeleting(true)
-              e.preventDefault()
-            }
-          })}} >
-           
-            <div>{item.name}</div>
-            <br />
-            <div className='id'>{item.id}</div>
-            <br />
-            <input type="submit" value="削除" />
-            <br />
-           
-          </form>
+
+          {isLoading? 
+          <div>削除中...</div>
+
+            :
+
+          <div>
+            削除しました。
+            {datas.map((item, index) => (
+              <form key={index} >
+              
+                <div>{item.name}</div>
+                <br />
+                <div className='id'>{item.id}</div>
+                <br />
+                <input onClick={(e)=>clickDelete(e,index)} type="submit" value="削除" />
+                <br />
+              
+              </form>
           ))}
-          {isDeleting?  <div>削除しました。</div> 
-          : 
-          <div>削除中...</div>}
+          </div> 
+          
+          }
           </div>
     }
 }
